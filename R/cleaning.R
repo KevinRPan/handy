5a1b9208ad897331f051afb571f13245dedcb239
Kevin Pan <kevinrpan@gmail.com>
2017-06-07 15:23:57 -0700

    add removeColumns and update documentation

--- R/cleaning.R
+++ R/cleaning.R
@@ -1,8 +1,3 @@
-#' Hello, world!
-#'
-#' This is an example function named 'hello'
-#' which prints 'Hello, world!'.
-#'
 #' You can learn more about package authoring with RStudio at:
 #'
 #'   http://r-pkgs.had.co.nz/
@@ -12,40 +7,57 @@
 #'   Build and Reload Package:  'Ctrl + Shift + B'
 #'   Check Package:             'Ctrl + Shift + E'
 #'   Test Package:              'Ctrl + Shift + T'
-#' importFrom magrittr "%>%"
+
+#' @importFrom magrittr "%>%"
 
 # exportPattern("^[[:alpha:]]+")
 devtools::use_package("dplyr")
 devtools::use_package("tidyverse")
 devtools::use_package("magrittr")
 devtools::use_package("stringr")
-# roxygen2::roxygenise()
-
-hello <- function() {
-  print("Hello, world!")
-}
 
 #' @title Standardize variable names
+#' @name make_names
 #'
 #' @description
-#' \code{make_names} returns a vector that has been normalized to lowercase separated by underscores
+#' Normalize names to lowercase separated by underscores from character vector.
 #'
-#' @param var_names vector of variable names
+#' @param names character vector to be coerced to syntactically valid names. This is coerced to character if necessary.
 #'
-#' @return cleaned up variable names
+#' @return A character vector of same length as \code{names} with each changed to a normalized variable name.
 #'
-#' @usage
 #' @export
-make_names <- function(var_names) {
+make_names <- function(names) {
   # Function to clean up column names
-  var_names %>%
-    str_trim %>%
+  names %>%
+    stringr::str_trim %>%
     tolower %>%
     make.names(unique = TRUE) %>%
-    str_replace_all('[.]', '_') %>%
-    str_replace_all('__{1,4}', '_') %>%
-    str_replace_all('_$', '') %>%
+    stringr::str_replace_all('[.]', '_') %>%
+    stringr::str_replace_all('__{1,4}', '_') %>%
+    stringr::str_replace_all('_$', '') %>%
     return
+}
+
+#' @title Remove columns based on a number of distinct values
+#'
+#' @description
+#' Remove columns with less than a specified number of distinct values.
+#'
+#' @param df the input dataframe
+#' @param .uniqueness the number of unique values the value should take on, omitting NAs.
+#'
+#' @return A dataframe with columns removed.
+#' Note: using 0 will remove columns where all observations are NA.
+#' @examples
+#' \dontrun{
+#' df <- head(data.frame('x' = letters, 'y' = rep(0, length(letters)), z = rep(NA, length(letters))))
+#' removeColumns(df)
+#' removeColumns(df, 0)
+#' }
+#' @export
+removeColumns <- function(df, .uniqueness = 1) {
+  return(df[df %>% purrr::map_lgl(~ length(unique(na.omit(.x)))>.uniqueness)])
 }
 
 #
---
+++ R/handy.R
@@ -0,0 +1,7 @@
+#' handy: A package that contains several commonly used functions for cleaning and examining data.
+#'
+#' The handy package provides some functions that I found handy.
+#'
+#' @docType package
+#' @name handy
+NULL
