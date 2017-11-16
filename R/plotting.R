1b5cd2882e8c88cbfdcd4491fa0ddd17055bc730
Kevin Pan <kevinrpan@gmail.com>
2017-11-16 15:20:18 -0800

    add multiplot

--- R/plotting.R
+++ R/plotting.R
@@ -5,6 +5,7 @@ devtools::use_package("dplyr")
 devtools::use_package("tidyverse")
 devtools::use_package("magrittr")
 devtools::use_package("stringr")
+devtools::use_package("grid")
 
 #' @title quarter_as_date
 #' @description Convert discrete year and quarter to date for plotting purposes.
@@ -28,8 +29,9 @@ quarter_as_date <- function(year, quarter, day = 1) {
 
 #' @title %p%
 #' @description Paste characters. Yay.
+#'
 #' @export
-`%p%` <- function(x1, x2) paste0(x1, x2)
+`%p%` <- function(x1, x2) {paste0(x1, x2)}
 
 # ---------------------------------------------------------------------------------------------
 # Formatting functions for ggplot  graph axis
@@ -106,3 +108,68 @@ human_num   <- function(x){human_numbers(x, smbl = "")}
 #' @export
 human_usd   <- function(x){human_numbers(x, smbl = "$")}
 
+
+#' @title Multiple plot function
+#' @description Format multiple plots in a grid layout
+#' If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+#' then plot 1 will go in the upper left, 2 will go in the upper right, and
+#' 3 will go all the way across the bottom.
+#'
+#' @return arrangement of plots
+#' @param ...    ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+#' @param cols   Number of columns in layout
+#' @param layout A matrix specifying the layout. If present, 'cols' is ignored.
+#' @examples
+#'
+#' library(ggplot2)
+#' This example uses the ChickWeight dataset, which comes with ggplot2
+#' First plot
+#' p1 <- ggplot(ChickWeight, aes(x=Time, y=weight, colour=Diet, group=Chick)) +
+#' geom_line() +
+#'  ggtitle("Growth curve for individual chicks")
+#'
+#' Second plot
+#' p2 <- ggplot(ChickWeight, aes(x=Time, y=weight, colour=Diet)) +
+#'  geom_point(alpha=.3) +
+#'  geom_smooth(alpha=.2, size=1) +
+#'  ggtitle("Fitted growth curve per diet")
+#'
+#' multiplot(p1, p2)
+#' @source http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+#' @export
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+  if (numPlots == 1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid::grid.newpage()
+
+    grid::viewport(layout = grid::grid.layout(nrow(layout), ncol(layout))) %>%
+      grid::pushViewport()
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = grid::viewport(layout.pos.row = matchidx$row,
+                                            layout.pos.col = matchidx$col))
+    }
+  }
+}
